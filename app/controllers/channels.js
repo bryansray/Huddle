50994a38cb310d1d5082f6fc1c738f1f9bcfd765
Bryan Ray <bryan@bryanray.net>
2015-07-07 08:15:28 -1000

    Start leveraging the mongo database for data.

--- app/controllers/channels.js
+++ app/controllers/channels.js
@@ -1,19 +1,15 @@
+var Channel = require('../models/channel');
+
 exports.index = function(req, res) {
-	var channels = [
-		{ channelId: 1, name: "General Discussion" },
-		{ channelId: 2, name: "Team Room 1" },
-		{ channelId: 3, name: "Team Room 2" }
-	];
-	
-	return res.json(channels);
+	Channel.find(function(err, channels) {
+		return res.json(channels);
+	});
 };
 
 exports.show = function(req, res) {
 	var channelId = req.params.id;
 
-	var channel = {
-		channelId: channelId, name: "General Discussion"
-	};
-
-	return res.json(channel);
+	Channel.findOne({ _id: channelId }, function(err, channel) {
+		return res.json(channel);
+	});
 };
