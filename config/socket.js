3806813c24f9088b97197dbb850a145cd6ee49de
Bryan Ray <bryan@bryanray.net>
2015-07-12 19:41:08 -1000

    Add in support for hashtags
    Strip out hashtag information and save it to the database.

--- config/socket.js
+++ config/socket.js
@@ -1,5 +1,7 @@
 var Room = require('../app/models/room');
 
+
+
 module.exports = function(server) {
 	var io = require('socket.io')(server),
 			markdown = require('markdown').markdown;
@@ -10,12 +12,14 @@ module.exports = function(server) {
 			var user = { id: 1, firstName: "Bryan", lastName: "Ray", displayName: "Bryan Ray", status: "active" };
 			var room = { id: data.roomId };
 
+			var message = `*** ${user.firstName} ${user.lastName} joined the Room.`;
+
 			var joinedEventData = { 
 				from_user: { displayName: "Huddle" },
 				user: user, 
 				room: room, 
-				message: `*** ${user.firstName} ${user.lastName} joined the Room.`,
-				html: `*** ${user.firstName} ${user.lastName} joined the Room.`,
+				message: message,
+				html: markdown.toHTML(message),
 				timestamp: new Date()
 			};
 			socket.join(data.roomId);
@@ -24,14 +28,20 @@ module.exports = function(server) {
 
 		// MESSAGE (RECEIVED) EVENT
 		socket.on('message', function(data) {
+			var regexHashtags = /(^|\s)(#[a-z\d-]+)/ig;
+
 			var message = data.message,
 					userId = data.userId,
 					roomId = data.roomId,
-					html = markdown.toHTML(message, 'Maruku'),
 					timestamp = new Date();
 
+			var tags = message.match(regexHashtags),
+					html = markdown.toHTML(message, 'Maruku').replace(regexHashtags, "$1<span class=\"hash-tag\">$2</span>");
+
+			if (tags) tags = tags.map(function(tag) { return tag.replace(/ /g, '').replace( /#/, ''); })
+
 			var user = { firstName: "Bryan", lastName: "Ray", displayName: "Bryan Ray" };
-			var message = { user: user, message: message, html: html, timestamp: timestamp };
+			var message = { user: user, message: message, html: html, tags: tags, timestamp: timestamp };
 
 			Room.Model.findById(roomId, function(err, room) {
 				room.messages.push(message)
