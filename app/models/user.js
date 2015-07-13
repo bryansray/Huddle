45d03df09cc446de63d78d3e95af56952d556bed
Bryan Ray <bryan@bryanray.net>
2015-07-13 08:47:35 -1000

    Adding in some user properties and methods.

--- app/models/user.js
+++ app/models/user.js
@@ -2,8 +2,16 @@ var mongoose = require('mongoose');
 
 var schema = mongoose.Schema({
 	firstName: String,
-	lastName: String
+	lastName: String,
+	displayName: String,
+	email: String,
+	salt: String,
+	hash: String
 });
+
+schema.methods.validatePassword = function(password) {
+	return true;
+};
 
 var User = mongoose.model('user', schema);
 
