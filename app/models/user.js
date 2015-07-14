960f908a4cd1dad03998765bb863084bd29851bb
Bryan Ray <bryan@bryanray.net>
2015-07-13 14:56:46 -1000

    Add in support for salt and password hash.

--- app/models/user.js
+++ app/models/user.js
@@ -1,16 +1,35 @@
-var mongoose = require('mongoose');
+var mongoose = require('mongoose'),
+		Schema = mongoose.Schema,
+		crypto = require('crypto-js');
 
-var schema = mongoose.Schema({
+var schema = new Schema({
 	firstName: String,
 	lastName: String,
 	displayName: String,
 	email: String,
 	salt: String,
-	hash: String
+	password: String
 });
 
-schema.methods.validatePassword = function(password) {
-	return true;
+schema.pre('save', function(next) {
+	if (this.password && this.password.length > 6) {
+		this.salt = crypto.lib.WordArray.random(128/8);
+		this.password = this.hashPassword(this.password);
+	}
+
+	next();
+});
+
+schema.methods.authenticate = function(password) {
+	return this.password === this.hashPassword(password);
+};
+
+schema.methods.hashPassword = function(password) {
+	if (this.salt && password) {
+		return crypto.PBKDF2(password, this.salt, { keySize: 512/32 }).toString();
+	} else { 
+		return password;
+	}
 };
 
 var User = mongoose.model('user', schema);
--- config/passport.js
+++ config/passport.js
@@ -2,17 +2,13 @@ var LocalStrategy = require('passport-local').Strategy,
 		User = require('../app/models/user');
 
 module.exports = function(app, config, passport) {
-	console.log("Configuring Passport ...");
-
 	// Configure Passport
 	passport.use(new LocalStrategy({ usernameField: 'email' },
 		function(email, password, done) {
-			console.log("Executing verification callback: ", arguments);
 			User.Model.findOne({ email: email}, function(err, user) {
-				console.log(err, user);
 				if (err) return done(err);
 				if (!user) return done(null, false, { message: "Incorrect Email or Password provided." });
-				if (!user.validatePassword(password)) return done(null, false, { message: "Incorrect Email or Password provied."});
+				if (!user.authenticate(password)) return done(null, false, { message: "Incorrect Email or Password provied."});
 
 				return done(null, user);
 			});
@@ -20,12 +16,10 @@ module.exports = function(app, config, passport) {
 	));
 
 	passport.serializeUser(function(user, done) {
-		console.log("Serializing user: ", arguments);
 	  done(null, user.id);
 	});
 
 	passport.deserializeUser(function(id, done) {
-		console.log("Deserializing user: ", arguments);
 	  User.Model.findById(id, function(err, user) {
 	    done(err, user);
 	  });
