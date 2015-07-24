764c674e9a44741b5b2e63d659f3dad4c45bd87b
Bryan Ray <bryan@bryanray.net>
2015-07-24 18:43:45 -0500

    Creating a user controller
    Fixing the swap from mongo to postgres

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -5,8 +5,8 @@ exports.new = function(req, res) {
 };
 
 exports.create = function(req, res) {
-	User.Model.create(req.body, function(err, user) {
-		if (err) res.render('users/new');
+	User.forge(req.body).save().then(function(user) {
+		if (user === null) res.render('users/new');
 		res.redirect('/login');
 	});
 };
