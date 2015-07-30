cda41c90a2d9c52cd29fa53dd66e45b624d6d24a
Bryan Ray <bryan@bryanray.net>
2015-07-29 21:43:11 -0500

    Let's have some better support for scrolling.

--- app/assets/javascript/components/roomMessages.js
+++ app/assets/javascript/components/roomMessages.js
@@ -22,6 +22,11 @@ var RoomMessagesComponent = Ractive.extend({
 		this.root.socket.on('message', _.bind(this.messageEvent, this));
 	},
 
+	oncomplete: function() {
+		var messagesContainer = this.find('#chat-messages');
+		messagesContainer.addEventListener('scroll', this.handleScrolling.bind(this));
+	},
+
 	getRoomMessages: function(room, oldRoom, keypath) {
 		if (!room) return;
 
@@ -38,6 +43,7 @@ var RoomMessagesComponent = Ractive.extend({
 		var message = this.get('messageInput'),
 				room = this.parent.get('activeRoom');
 		this.root.socket.emit('message', { userId: this.root.get('current_user.id'), roomId: room.id, message: message });
+		this.scrollToTop();
 	},
 
 	handleTyping: function(event) {
@@ -58,14 +64,30 @@ var RoomMessagesComponent = Ractive.extend({
 	},
 
 	scrollToTop: function() {
-		var messagesElement = document.getElementById('chat-messages');
+		var messagesElement = this.find('#chat-messages');
 		messagesElement.scrollTop = messagesElement.scrollHeight;
+	},
+
+	handleScrolling: function(event) {
+		var percentage = this._scrollPercentage(event.target);
+		// TODO : When we get to a certain percentage we should load more.
+		if (percentage < 20) {
+			console.log("Start loading more historical messages ...");
+		}
 	},
 
 	messageEvent: function(data) {
 		var messages = this.get('messages');
 		messages.push(data);
-		this.scrollToTop();
+
+		if (this._scrollPercentage() >= 98)
+			this.scrollToTop();
+	},
+
+	_scrollPercentage: function(element) {
+		if (!element) element = this.find('#chat-messages');
+
+		return 100 * element.scrollTop / ( element.scrollHeight  - element.clientHeight );
 	}
 });
 
