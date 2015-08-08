d81dcf9aac08963fa6090f575f6352df7db2fb62
Bryan Ray <bryan@bryanray.net>
2015-08-07 21:31:09 -0500

    Cleaning up old refactoring code.

--- app/models/room.js
+++ app/models/room.js
@@ -1,5 +1,4 @@
 var Bookshelf = require('../../config/bookshelf');
-console.log("Room.JS ...");
 
 var Room = Bookshelf.Model.extend({
 	tableName: 'rooms',
@@ -14,38 +13,3 @@ var Room = Bookshelf.Model.extend({
 });
 
 module.exports = Bookshelf.model('Room', Room);
-// // var mongoose = require('mongoose');
-
-// var schema = mongoose.Schema({
-// 	name: String,
-// 	description: String,
-// 	users: [mongoose.Schema.Types.ObjectId],
-// 	messages: []
-// });
-
-// schema.methods.addUser = function(user) {
-// 	var index = this.users.indexOf(user.id);
-// 	if (index === -1)
-// 		this.users.push(user.id);
-
-// 	return this;
-// };
-
-// schema.methods.removeUser = function(user) {
-// 	var index = this.users.indexOf(user.id);
-// 	if (index > -1) 
-// 		this.users.splice(index, 1);
-	
-// 	return this;
-// };
-
-// schema.methods.addMessage = function(user, message) {
-// 	this.messages[message.id] = { user: user, message: message };
-// };
-
-// var Room = mongoose.model('room', schema);
-
-// module.exports = {
-// 	Schema: schema,
-// 	Model: Room
-// };
