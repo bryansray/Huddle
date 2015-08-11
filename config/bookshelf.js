334092bd9ec0ae5feb9d119c1a029a82542af404
Bryan Ray <bryan@bryanray.net>
2015-08-11 14:34:53 -0500

    Don't hardcode this db configuration.

--- config/bookshelf.js
+++ config/bookshelf.js
@@ -1,13 +1,5 @@
-var knex = require('knex')({
-	client: 'pg',
-	connection: {
-		host: '127.0.0.1',
-		user: 'bryanray',
-		password: '',
-		database: 'huddle',
-		charset: 'utf8'
-	}
-});
+var config = require('./config'),
+		knex = require('knex')(config.db);
 
 var Bookshelf = require('bookshelf')(knex);
 
